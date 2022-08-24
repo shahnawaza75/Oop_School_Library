4ca58781a912d987bfd00cb14862ac42e6071ad4
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-08-24 20:56:51 +0530

    Fix linters

--- person.rb
+++ person.rb
@@ -1,34 +1,31 @@
-require './nameable'
-class Person
+require_relative 'nameable'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-decorate-class
+  def initialize(age, name = 'Unknown', _parent_permission: true)
     super()
-  def initialize(age, name = 'Unknown', parent_permission: true)
-
     @id = Random.rand(1..1000)
     @name = name
     @age = age
-    @parent_permission = parent_permission
-  end
-
-  def can_use_services?
-    return true if of_age? || @parent_permission
-
-    false
   end
 
   private
 
-  def correct_name
-    @name
+  def of_age?
+    @age >= 18
   end
+
+  public
 
   def can_use_services?
     return true if is_of_age?
+  end
 
-  def of_age?
-    @age >= 18
+  def correct_name
+    @name
   end
 end
