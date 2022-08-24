5a3c4dc30b2e2a7e0d02670c5b508d0bdf9393e7
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-08-24 18:17:43 +0530

    Added classes

--- person.rb
+++ person.rb
@@ -1,8 +1,10 @@
+require './nameable'
 class Person
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', _parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -15,6 +17,10 @@ class Person
   end
 
   public
+  
+  def correct_name
+    @name
+  end
 
   def can_use_services?
     return true if is_of_age?
