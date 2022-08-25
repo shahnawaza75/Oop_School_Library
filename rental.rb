6580072a4e267b832cdf645b10029fd0d731540e
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-08-25 13:24:20 +0530

    Fix linters

--- rental.rb
+++ rental.rb
@@ -1,12 +1,12 @@
 class Rental
-    attr_accessor :date
-    attr_reader :book, :person
-  
-    def initialize(date, person, book)
-      @date = date
-      @person = person
-      person.rental << self
-      @book = book
-      book.rental << self
-    end
+  attr_accessor :date
+  attr_reader :book, :person
+
+  def initialize(date, person, book)
+    @date = date
+    @person = person
+    person.rental << self
+    @book = book
+    book.rental << self
   end
+end
